536e65de576970d814709f6dd01d9f26a867bd64
Bfas237 <gwatanajohn3@gmail.com>
2019-04-08 08:19:40 +0100

    ...

--- bots.js
+++ bots.js
@@ -15,9 +15,9 @@ console.log('Bot server started in the ' + process.env.NODE_ENV + ' mode');
 
 bot.on('message', (msg) => {
   const name = msg.from.first_name;
-  bot.sendMessage(msg.chat.id, 'Hello, ' + name + '!').then(() => {
+  bot.sendMessage(msg.chat.id, 'Hello, ' + name + '! how are you').then(() => {
     // reply sent!
   });
 });
- 
+
 module.exports = bot;
